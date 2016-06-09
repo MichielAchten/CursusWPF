0bfa5722655026e5807b2b722f9ded0bdb5de12b
MichielAchten <michiel.achten@gmail.com>
2016-06-09 10:39:15 +0200

    ParkingBon1

--- ParkingBon/Properties/AssemblyInfo.cs
+++ ParkingBon/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OefeningParkingbon")]
+[assembly: AssemblyTitle("ParkingBon")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OefeningParkingbon")]
+[assembly: AssemblyProduct("ParkingBon")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ParkingBon/Properties/Settings.Designer.cs
+++ ParkingBon/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OefeningParkingbon.Properties
+namespace ParkingBon.Properties
 {
 
 
